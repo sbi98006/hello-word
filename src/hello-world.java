c297593576f64226005e3795df080933fe204fad
sbi98006 <sbi98006@yahoo.com>
2017-06-22 18:11:15 -0700

    Delete hello-world.java

--- src/hello-world.java
+++
@@ -1,9 +0,0 @@
-package SteveHelloWorld;
-
-public class hello-world 
-{  
-  public static void main(String[] args) 
-  {      
-    System.out.println("Hello, World - This is Steve from Travis CI!");    
-  }
-}
