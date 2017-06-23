595040805de11dfdb8e775c6df8040f282cececb
sbi98006 <sbi98006@yahoo.com>
2017-06-22 18:08:29 -0700

    Update hello-world.java

--- src/hello-world.java
+++ src/hello-world.java
@@ -1,9 +1,9 @@
 package SteveHelloWorld;
 
-public class HelloWorld 
+public class hello-world 
 {  
   public static void main(String[] args) 
   {      
-    System.out.println("Hello, World");    
+    System.out.println("Hello, World - This is Steve from Travis CI!");    
   }
 }
